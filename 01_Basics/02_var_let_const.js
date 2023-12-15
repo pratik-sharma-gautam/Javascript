6dc3ce911058125d61808319283a038dbfe2633d
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-15 15:26:02 +0000

    Let, Const, and Var in JS

---
+++ 01_Basics/02_var_let_const.js
@@ -0,0 +1,21 @@
+const PI=3.14 // This Value PI cannot be changed
+console.log(PI)
+
+// PI=3.2
+
+let age=18;
+console.log(age)
+
+age=20
+console.log(age)
+
+{
+    var a=5 // You shouldnot be using var most of the times or try to avoid it
+}
+console.log(a)
+
+
+console.log(5+5)
+console.log(5%2)
+
+console.log(5>13)
