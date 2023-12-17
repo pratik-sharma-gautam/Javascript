acb48fde99a602b92133165b8a738ae27d3f054a
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-17 02:11:42 +0000

    Datatype Conversion

---
+++ 01_Basics/04_datatype_conversion.js
@@ -0,0 +1,47 @@
+// Datatype conversion
+/*
+String to Number
+String to Boolean
+Boolean to Number
+Boolean to String
+Number to String
+Number to Boolean
+*/
+
+// let a="js"
+// let b=Number(a)
+// console.log(typeof b)
+// console.log(b)
+
+// let myEmptyString=""
+// let n=Number(myEmptyString)
+// console.log(n, typeof n)
+
+// let StringA=""
+// let stringToBoolean=Boolean(StringA)
+// console.log(stringToBoolean, typeof stringToBoolean)
+
+// let id=false
+// let NumberFromBool=Number(id)
+// console.log(NumberFromBool, typeof NumberFromBool)
+
+// let id=true
+// let boolToString=String(id)
+// console.log(id, boolToString, typeof boolToString)
+
+// let number=5
+// let myNumber=String(number)
+// console.log(myNumber, typeof myNumber)
+
+// let number=-5
+// console.log(Boolean(number), typeof Boolean(number))
+
+// Operator: +,-,*,/,%  ||  **, !, >, <, <=, >=, ==, ===
+
+// console.log(2**3, 2!=2, 2==5, 2>3, 2<1, 2>=2)
+
+
+let a="5"
+let b=5
+
+console.log(a===b)
