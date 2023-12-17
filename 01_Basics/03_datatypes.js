4fe176523d0e44c82dc2be216606506bfe45414b
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-17 02:11:23 +0000

    Datatypes in JS

---
+++ 01_Basics/03_datatypes.js
@@ -0,0 +1,35 @@
+// let a="name"
+// let b=5
+// console.log(a+b+5)
+
+// Datatypes - String, Number, BigInt, Boolean, Undefined, Null,Symbol, Object
+
+// let a="a"
+// console.log(typeof a)
+// let b=56
+// let c=7.904
+
+// console.log(typeof b +" "+ typeof c)
+// let x=BigInt("3847328947328573");
+// console.log(x, typeof x)
+
+// let d=true
+// let e=false
+// console.log(d,typeof d, e, typeof e)
+
+// let name
+// console.log(typeof name)
+
+// let b=null
+// console.log(typeof null)
+
+// let a={
+//     fName:"Bob",
+//     lName:"M"
+// }
+// console.log(a.lName)
+// a.lName="P"
+// console.log(a)
+
+let arr=[5,6,7,"String"]
+console.log(arr)
