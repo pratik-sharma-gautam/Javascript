75ddb762c789efc202e7da9d789c0efc0e81c36a
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-18 04:05:11 +0000

    Strings Functions

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,48 @@
+// let name="pratik"
+// console.log(name, typeof name)
+
+// let name1=new String();
+// let num=new Number();
+// console.log(name1, typeof name1, num, typeof num)
+
+
+// let fName="pratik"
+//            654321
+// let lName="ShaRma"
+
+// console.log("My Name is "+fName+" "+lName);
+
+// console.log(`My Name is ${fName} ${lName}`)
+
+// console.log(fName.length);
+// console.log(fName.toUpperCase())
+// let b=lName.toLowerCase();
+// console.log(b)
+
+// console.log(fName.charAt(1))
+// console.log(fName.indexOf('t'))
+
+
+// let myString=fName.substring(0, 3);
+// console.log(myString)
+
+// let myNewString=fName.slice(-5,-2);
+// console.log(myNewString);
+
+// let z="            Name                 "
+// console.log(z.trim());
+
+
+// let x="My Name is pratik"
+// let y=x.replace("Name", "first name")
+// console.log(y);
+
+
+// console.log(x.includes('isa'))
+
+
+let zz="My-Name-is-bla-bla-bla"
+let yy="This is the string that contains spaces"
+
+console.log(zz.split("-"))
+console.log(yy.split(" "))
