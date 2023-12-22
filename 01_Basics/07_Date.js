451343aeb558a9246c163ee782117f9439d33f8f
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-22 13:17:49 +0000

    Date

---
+++ 01_Basics/07_Date.js
@@ -0,0 +1,17 @@
+
+//Date
+
+// let myDate=new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+let a=new Date()
+console.log(a.getDay())
+console.log(a.getMonth())
+a=Date.now()
+// console.log(Math.round(a/1000))
+
