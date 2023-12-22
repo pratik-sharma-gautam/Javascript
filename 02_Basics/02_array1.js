d45beeee838283bf47be82c80eb0b8f312a6274e
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-22 13:18:04 +0000

    Array - Part1

---
+++ 02_Basics/02_array1.js
@@ -0,0 +1,31 @@
+// Array -> Collection of Elements
+
+// let arr=[1,2,3]
+
+// console.log(arr[1])
+
+const arr1=new Array(1,2,3,5,6)
+// console.log(arr1[2])
+
+// arr1.push(6)
+// console.log(arr1)
+
+// arr1.pop()
+// console.log(arr1)
+
+// arr1.unshift(5)
+// console.log(arr1)
+// arr1.shift() // Removes the first element
+
+
+// console.log(arr1.includes(9));
+
+// console.log(arr1.indexOf(9))
+
+// Splice & Slice
+
+// console.log(arr1.slice(1,3));
+// console.log(arr1)
+
+console.log(arr1.splice(1,3))
+console.log(arr1)
