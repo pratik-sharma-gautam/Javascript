860ebc2a23dda82587d9fcdd8c68198eb352a31a
Pratik Sharma <141541302+pratik-sharma-gautam@users.noreply.github.com>
2023-12-22 13:17:23 +0000

    Maths

---
+++ 01_Basics/06_Maths.js
@@ -0,0 +1,36 @@
+// Numbers
+
+// let a=100
+
+// console.log(a);
+// console.log(a.toString())
+// let b=55.749
+// console.log(b.toFixed(1))
+
+let c=100000000000
+// console.log(c.toPrecision(3));
+
+console.log(c.toLocaleString());
+
+
+// Maths
+
+a=8.01
+
+// console.log(Math.abs(a));
+
+// console.log(Math.round(a))
+
+// console.log(Math.floor(a));
+
+// console.log(Math.ceil(a))
+a=5
+b=7
+// console.log(Math.max(a,b))
+
+console.log((Math.random()).toFixed(1))
+
+
+
+
+
